1001168a1a528249ead22151af71baa900e22c60
sy553 <sangyuenan@gmail.com>
2024-11-29 23:00:01 +0800

    修复 CORS 问题

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -4,7 +4,6 @@ import { ElMessage } from 'element-plus'
 const service = axios.create({
   baseURL: 'http://localhost:5000',
   timeout: 10000,
-  withCredentials: true
 })
 
 service.interceptors.request.use(
